0fb9c3ac3d4e60d61a96fabe807d385b0b4eede0
2nature <idontneedmorelight@gmail.com>
2023-11-27 10:00:16 +0900

    second

---
+++ git/pet/src/main/java/com/wproject/pet/dto/Member.java
@@ -0,0 +1,30 @@
+package com.wproject.pet.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int member_id;
+	@NotNull
+	private String userid;
+	@NotNull
+	private String password;
+	@NotNull
+	private String name;
+	@NotNull
+	private int tel;
+	@NotNull
+	private String address;
+	@NotNull
+	private String email;
+}
