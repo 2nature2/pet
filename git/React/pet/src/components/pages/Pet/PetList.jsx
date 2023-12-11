9cc1f1384ccf79e6c338a23655dc9e699e008972
treatmeluna <realizemiyu@gmail.com>
2023-12-11 15:20:56 +0900

    dockerfile / Pet pages upload

---
+++ git/React/pet/src/components/pages/Pet/PetList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import '../../styles/Community.css';
+
+const PetList = () => {
+    
+
+    return (
+        <div>
+            
+            <span>공고번호</span>
+            <span>접수일자</span>
+            <span>품종</span>
+            <span>성별</span>
+            <span>발견장소</span>
+            <span>특징</span>
+            <span>상태</span>
+            <span>공고기간</span>
+
+        </div>
+    )
+}
+
+export default PetList;
--- git/React/pet/src/components/pages/Pet/PetMain.jsx
+++ git/React/pet/src/components/pages/Pet/PetMain.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../styles/Community.css';
 
-const PetMain = ({pets}) => {
+const PetMain = () => {
     const navigate = useNavigate();
 
-    const {
+    // const {
         
-        noticeNo, // "경남-창원1-2023-00644"
-        noticeSdt, // 접수일시
-        popfile, // img
-        happenPlace, // 발견장소
-        kindCd, // 종류
-        specialMark, // 특징
-        processState, // 상태(보유중 보호중 등등..)
+    //     noticeNo, // "경남-창원1-2023-00644"
+    //     noticeSdt, // 접수일시
+    //     popfile, // img
+    //     happenPlace, // 발견장소
+    //     kindCd, // 종류
+    //     specialMark, // 특징
+    //     processState, // 상태(보유중 보호중 등등..)
 
-    } = pets;
+    // } = pets;
 
     return (
-        <div>
+        <div className='community'>
+            <div className='cboard'>
+            <h2>기간이 얼마 남지 않은 아이들이에요.</h2>
+            </div>
             
         </div>
     )
