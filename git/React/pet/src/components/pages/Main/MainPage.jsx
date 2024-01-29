a688ad5b79debc6a25bb913bb4c23fa1fc382eb4
2nature <idontneedmorelight@gmail.com>
2024-01-29 15:23:37 +0900

    메인 수정2

--- git/React/pet/src/components/pages/Main/MainPage.jsx
+++ git/React/pet/src/components/pages/Main/MainPage.jsx
@@ -4,7 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import PetSlider from '../Pet/PetSlider.jsx';
-import { Button, Table } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
 const MainPage = ({bests}) => {
     console.log('bests', bests);
@@ -29,7 +29,6 @@ const MainPage = ({bests}) => {
         className: "center",
         centerMode: true,
         infinite: true,
-        centerPadding: "60px",
         slidesToShow: 3,
         swipeToSlide: true,
         arrows: false, 
@@ -37,6 +36,30 @@ const MainPage = ({bests}) => {
 
     return (
         <div className='mainBoard'>
+            <div className='first-container'>
+            {/* <PetSlider /> */}
+            </div>
+            <div className='second-container'>
+            <div className='bestCommunity'>
+                <span className='sub'>인기글</span>
+                <Table className='best'>
+                    <tbody>
+                        {
+                            bests.map((best, index)=> (
+                                index%2 === 0 
+                                ?
+                                <tr>
+                                    <td style={{borderBottomColor:'#b80042'}} key={index}><span className='emj'>덍 </span><a href={`/community/view/${best.bnum}`}><span>[{best.b_category}] </span>{best.b_title} <span style={{color:'gray'}}>{`\n`}[{best.b_comments}]</span></a></td>
+                                </tr>
+                                :
+                                <tr>
+                                    <td style={{borderBottomColor:'#b80042'}} key={index}><span className='emj'>넁 </span><a href={`/community/view/${best.bnum}`}><span>[{best.b_category}] </span>{best.b_title} <span style={{color:'gray'}}>{`\n`}[{best.b_comments}]</span></a></td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </Table>
+            </div>
             <div className='contest'>
                 <div className='contestTxt'>반려동물을 자랑해주세요!</div>
                 <div className='constestImgs'>
@@ -50,35 +73,17 @@ const MainPage = ({bests}) => {
                         }
                     </Slider>
                 </div>
-               
+                <div className='contestSubTxt'>커뮤니티에서 자랑 카테고리로 등록하시면 운영자가 확인 후 등록합니다.</div>
             </div>
-            <div className='bestCommunity'>
-                <span className='sub'>인기글</span>
-                <Table className='best'>
-                    <tbody>
-                        {
-                            bests.map((best, index)=> (
-                                index%2 == 0 
-                                ?
-                                <tr>
-                                    <td style={{borderBottomColor:'#b80042'}} key={index}><span className='emj'>덍 </span><a href={`/community/view/${best.bnum}`}><span>[{best.b_category}] </span>{best.b_title} <span style={{color:'gray'}}>{`\n`}[{best.b_comments}]</span></a></td>
-                                </tr>
-                                :
-                                <tr>
-                                    <td style={{borderBottomColor:'#b80042'}} key={index}><span className='emj'>넁 </span><a href={`/community/view/${best.bnum}`}><span>[{best.b_category}] </span>{best.b_title} <span style={{color:'gray'}}>{`\n`}[{best.b_comments}]</span></a></td>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </Table>
-                
+            <div className='testbtns'>
+                <button className='testbtn1' onClick={() => window.open('https://AlwayWithAnimalmbti.waveon.io', '_blank')}>
+                    <img src='../img/mbtitest.png' style={{width:'100%', height:'100%'}}/>
+                </button>
+                <button className='testbtn2'>
+                    To be continued
+                </button>
             </div>
-            {/* <PetSlider /> */}
-            
-            <button className='testbtn' onClick={() => window.open('https://AlwayWithAnimalmbti.waveon.io', '_blank')}>
-                <img src='../img/mbtitest.png' style={{width:'100%', height:'100%'}}/>
-            </button>
-
+            </div>
         </div>
        
     );
