cf0538fa292c4a3a7bc2eb22adc9b0cbc9df2881
2nature <idontneedmorelight@gmail.com>
2024-02-02 11:13:54 +0900

    merge 수정 완료

--- git/React/pet/src/components/pages/Enjoy/Enjoy.jsx
+++ git/React/pet/src/components/pages/Enjoy/Enjoy.jsx
@@ -2,28 +2,23 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import '../../styles/Enjoy.css';
 
-
 const Enjoy=()=>{
-
     
     return(
 
-        <>
         <Container className='enjoyTap'>
           <div className='game'>
             <a href='/enjoy/shooting'>
             <img src='../../gameimg/shooting.png' style={{width:'100%', height:'100%'}}/>
             <span>SHOOTING 게임 하러가기</span>
             </a>
+            <a href='/enjoy/merge'>
+            <img src='../../merge/catcatmerge.png' style={{width:'100%', height:'100%', backgroundColor:'white'}}/>
+            <span>CAT CAT MERGE 하러가기</span>
+            </a>
           </div>
         </Container>
-        <Container>
-          <a href='/enjoy/merge' style={{ display: 'flex', flexDirection: 'column', textDecoration: 'none', width:'200px', height:'200px' }}>
-          {/* <img src='../../gameimg/shooting.png' style={{width:'200px', height:'200px'}}/> */}
-          <span style={{ marginTop: '10px' }}>Merge 게임 하러가기</span>
-          </a>
-        </Container>
-        </>
+
     )
 }
 
